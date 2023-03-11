ba1102e7e2c72510ab1d57748ea23a53d7d2bdc8
Oloruntobi Olurombi <statemanui@gmail.com>
2023-03-11 14:10:46 +0000

    adjust

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,13 +1,19 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
+    # xray ------
+    segment = xray_recorder.begin_segment('user_activities')
     model = {
       'errors': None,
       'data': None
     }
 
     now = datetime.now(timezone.utc).astimezone()
-
+    dict = {
+      "now": now.isoformat()
+    }
+    
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
     else:
@@ -19,5 +25,7 @@ class UserActivities:
         'created_at': (now - timedelta(days=1)).isoformat(),
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
-      model['data'] = results
-    return model
+    model['data'] = results
+  subsegment = xray_recorder.begin_subsegment('mock-data')
+  subsegment.put_metadata('key', dict, 'namespace')  
+  return model
